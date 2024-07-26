896192b57dce437f50ca8f2d402c236569486532
DEIVYCALDERON <DEIVYS.CALDERONM@PCA.EDU.CO>
2024-07-26 09:34:29 -0500

    Diseño Menu

---
+++ src/app/menu/menu-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MenuPage } from './menu.page';
+import { HomePage } from '../home/home.page';
+import { LoginGuard } from '../guards/login.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MenuPage,
+    children:[
+      {
+      path:"home",
+      loadChildren:()=>import("../home/home.module").then( m => m.HomePageModule),canActivate:[LoginGuard]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MenuPageRoutingModule {}
